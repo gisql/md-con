68a9c3f2b48508b2fe9f75173f68e9d9da90f149
Michal Ostapowicz <ostapowicz.m@gmail.com>
2014-07-17 23:09:36 +0100

    Changing to method reference in config controller

--- app/scripts/services/md-config.js
+++ app/scripts/services/md-config.js
@@ -12,14 +12,18 @@
  * Factory in the mdConApp.
  */
 angular.module('mdConApp')
-    .factory('mdConfig', ['$http', function ($http) {
+    .factory('mdConfig', ['$http', '$log', function ($http, $log) {
+        $log.info('creating config factory');
+
         function requestConfig() {
+            $log.info('requesting config');
             return $http.get('/api/nav.json').then(function (res) {
+                $log.info('resolving config: %o', res.data);
                 return res.data;
             });
         }
 
         return {
-            states: requestConfig()
+            states: requestConfig
         };
     }]);
